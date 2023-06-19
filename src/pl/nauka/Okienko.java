aa4c114b0b70b328ceebd03170979bf46a9b8cc8
Kacper <repcakgonc@gamil.com>
2023-06-19 17:04:11 +0200

    Second commit

--- src/pl/nauka/Okienko.java
+++ src/pl/nauka/Okienko.java
@@ -1,2 +1,13 @@
-package pl.nauka;public class Okienko {
+package pl.nauka;
+
+import javax.swing.*;
+
+public class Okienko {
+    public void okienko(){
+        JFrame f=new JFrame("Kalkulator");
+        f.setSize(400,400);
+        f.setLayout(null);
+        f.setVisible(true);
+        new Przyciski(f);
+    }
 }
--- src/pl/nauka/Przyciski.java
+++ src/pl/nauka/Przyciski.java
@@ -1,2 +1,11 @@
-package pl.nauka;public class Przyciski {
+package pl.nauka;
+
+import javax.swing.*;
+
+public class Przyciski {
+    public Przyciski(JFrame f) {
+        JButton Dodaj=new JButton("Click Here");
+        Dodaj.setBounds(50,100,95,30);
+        f.add(Dodaj);
+    }
 }
