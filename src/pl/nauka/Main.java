1ce0c4877c20736890e9fdd22af1181663771b2c
Kacper <repcakgonc@gamil.com>
2023-06-20 19:47:14 +0200

    przycisk, logo, wpis

--- src/pl/nauka/Main.java
+++ src/pl/nauka/Main.java
@@ -11,10 +11,14 @@ public class Main {
         JFrame frm;
         Icon icon = new ImageIcon("C:\\Users\\kacpe\\OneDrive\\Pulpit\\kalkulator\\src\\pl\\nauka\\logo.png");
         btn = new JButton(icon);
-        frm = new JFrame("kalkulator");
-        btn.setBounds(40,40,180,180);
-        frm.setSize(500, 500);
+        frm = new JFrame("MathBooster");
+        tf = new JTextField();
+        frm.setIconImage(((ImageIcon) icon).getImage());
+        tf.setBounds(10, 20, 480, 80);
+        btn.setBounds(10,110,180,180);
+        frm.setSize(515, 500);
         frm.add(btn);
+        frm.add(tf);
         frm.setLayout(null);
         frm.setVisible(true);
     }
